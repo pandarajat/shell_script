860c916a7c8925f67dfad1d0ac073b0fe03c3f25
RAJA9658 <rajatpanda472@gmail.com>
2022-11-22 19:14:25 +0530

    inital commit

---
+++ 7max_dir.sh
@@ -0,0 +1,20 @@
+LOCATION=/tmp/dir
+FILECOUNT=$(find $LOCATION -maxdepth 1 -type f | wc -l)
+DIRCOUNT=$(find $LOCATION -maxdepth 1 -type d | wc -l)
+echo " files are: $FILECOUNT "
+echo "directories are: $DIRCOUNT "
+
+for ((a=$DIRCOUNT; a>=7; a--))
+do
+FILENAME=$(ls -t1 $LOCATION | tail -n 1)
+ echo "$LOCATION/$FILENAME"
+ rm -rf $LOCATION/$FILENAME
+ echo " $FILENAME"
+ echo " $FILENAME is delete"
+ DIRCOUNT=$(find $LOCATION -maxdepth 1 -type d | wc -l)
+
+echo  "$DIRCOUNT"
+done
+
+
+   
