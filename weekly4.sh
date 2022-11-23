02105bc9ad63841525d45f0962cff94bab2f305b
RAJA9658 <rajatpanda472@gmail.com>
2022-11-23 13:11:29 +0530

    Create weekly4.sh

---
+++ weekly4.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+LOCATION=/tmp/dir
+FILECOUNT=$(find $LOCATION -maxdepth 1 -type f | wc -l)
+DIRCOUNT=$(find $LOCATION -maxdepth 1 -type d | wc -l)
+echo " files are: $FILECOUNT "
+echo "directories are: $DIRCOUNT "
+
+for ((a=$DIRCOUNT; a>4; a--))
+do
+FILENAME=$(ls -t1 $LOCATION | tail -n 1)
+ echo "$LOCATION/$FILENAME"
+ sudo rm -rf $LOCATION/$FILENAME
+ echo " $FILENAME"
+ echo " $FILENAME is delete"
+ DIRCOUNT=$(find $LOCATION -maxdepth 1 -type d | wc -l)
+
+echo  "$DIRCOUNT"
+done
+echo "it needs more backup"
