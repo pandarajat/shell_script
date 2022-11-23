94c5571ecebd1c81032bf5c20a9d6effcbf53096
RAJA9658 <rajatpanda472@gmail.com>
2022-11-23 13:35:30 +0530

    Update 7max_dir.sh

--- 7max_dir.sh
+++ 7max_dir.sh
@@ -1,22 +1,23 @@
-#!/bin/bash 
+#!/bin/bash
 LOCATION=/tmp/dir
 FILECOUNT=$(find $LOCATION -maxdepth 1 -type f | wc -l)
 DIRCOUNT=$(find $LOCATION -maxdepth 1 -type d | wc -l)
 echo " files are: $FILECOUNT "
 echo "directories are: $DIRCOUNT "
 
-for ((a=$DIRCOUNT; a>=7; a--))
+for ((a=$DIRCOUNT; a>7; a--))
 do
-FILENAME=$(ls -t1 $LOCATION | tail -n 1)
- echo "$LOCATION/$FILENAME"
-sudo  rm -rf $LOCATION/$FILENAME
- echo " $FILENAME"
- echo " $FILENAME is delete"
+DIRNAME=$(ls -t1 $LOCATION | tail -n 1)
+ echo "$LOCATION/$DIRNAME"
+ sudo rm -rf $LOCATION/$DIRNAME
+ echo " $DIRNAME"
+ echo " $DIRNAME is delete"
  DIRCOUNT=$(find $LOCATION -maxdepth 1 -type d | wc -l)
 
-echo  "$DIRCOUNT"
+echo  "Reset-directories are : $DIRCOUNT "
 done
 echo "it needs more backup"
+
 
 
 
