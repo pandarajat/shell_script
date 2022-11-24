c1d3772b741736279bf475081667a802387321e7
RAJA9658 <rajatpanda472@gmail.com>
2022-11-24 15:28:18 +0530

    Create updatehttp.sh

---
+++ updatehttp.sh
@@ -0,0 +1,22 @@
+#post for update-schema
+API=$(curl  -X  POST  https://testing-cloud.imprintnext.io/api/v1/saas/update-schema -d '{"auth_token":"VFFxaE1qUGx2SE1oVmZ2TDk4SUh5QT09","version":"2.3","update_for":"all"}' | jq -r  '.remaining' )
+echo "$API"
+
+for (( i=$API; i>0; i-- ))
+ do
+ echo "run command for udate_schema "
+    curl  -X  POST  https://testing-cloud.imprintnext.io/api/v1/saas/update-schema -d '{"auth_token":"VFFxaE1qUGx2SE1oVmZ2TDk4SUh5QT09","version":"2.3","update_for":"all"}'
+done
+
+
+
+# post for update-lang
+
+LANGAPI=$(curl -X POST https://testing-cloud.imprintnext.io/api/v1/saas/update-lang -d '{"auth_token":"VFFxaE1qUGx2SE1oVmZ2TDk4SUh5QT09","version":"2.3","update_for":"all"}' | jq -r  '.merchant_remains' )
+echo "$LANGAPI"
+for (( i=$LANGAPI; i>0; i-- ))
+ do
+ echo "run command for update_lang"
+ curl -X POST https://testing-cloud.imprintnext.io/api/v1/saas/update-lang -d '{"auth_token":"VFFxaE1qUGx2SE1oVmZ2TDk4SUh5QT09","version":"2.3","update_for":"all"}'
+done
+echo "Script execution end"
